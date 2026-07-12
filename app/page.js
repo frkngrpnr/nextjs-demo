53a2bf6c23dc5ec13558b08ddd62e900a84a1b3e
uzayli <uz@ay.li>
2026-07-12 10:58:51 +0300

    initial commit

---
+++ app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "system-ui, sans-serif",
+      }}
+    >
+      <h1>Hello, World! 👋</h1>
+    </main>
+  );
+}
